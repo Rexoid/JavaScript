fc1c03674603a4f54b3c4cf9549c81aa90a7e423
Rexoid <31225964+Rexoid@users.noreply.github.com>
2017-08-22 11:24:30 +1200

    Add files via upload

---
+++ Platformer.js
@@ -0,0 +1,75 @@
+(function() {
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    window.requestAnimationFrame = requestAnimationFrame;
+})();
+Request_JSON();
+
+function Request_JSON() {
+	var Level_Layout_JSON = "";
+	var Level_Assets_JSON = "";
+	var request_Layout = new XMLHttpRequest();
+	var request_Assets = new XMLHttpRequest();
+	request_Layout.open("GET", Level_Layout_JSON);
+	request_Layout.responceType = "json";
+	request_Layout.send();
+	request_Layout.onload = function() {
+		var level_layout = request_Layout.responce;
+		Check_Active(level_layout);
+	}
+	request_Assets.open("GET", Level_Assets_JSON);
+	request_Assets.responceType = "json";
+	request_Assets.send();
+	request_Assets.onload = function() {
+		var level_assets = request_Assets.responce;
+		Generate_Assets(level_assets);
+	}
+}
+
+function Check_Active(levels_all) {
+	var levels = levels_all["levels"];
+	for(var i=0; i<levels.length; i++) {
+		var check_active = levels[i].Active;
+		if(check_active == "True") {
+			var Selected_Level = levels[i].layout;
+			Generate_Level(Selected_Level);
+		}
+	}
+}
+
+function Generate_Level(Selected_Level_Layout) {
+	var layout = Selected_Level_Layout;
+	for(var i=0; i<layout.length; i++) {
+		var object = layout.slice(i,i);
+		if(object == "2") {
+			var block_object = document.createElement("img");
+			block_object.id = "i";
+			block_object.className = "Block";
+			var canvas = document.getElementById("Level");
+			canvas.appendChild(block_object);
+			block_object.style.width = 50;
+			block_object.style.height = 50;
+			block_object.style.position = "absolute";
+			block_object.style.left = (i*50)+"px";
+			if(i<21) { block_object.style.top = 0+"px"; }
+			if(i<41) { block_object.style.top = 50+"px"; }
+			if(i<61) { block_object.style.top = 100+"px"; }
+			if(i<81) { block_object.style.top = 150+"px"; }
+			if(i<101) { block_object.style.top = 200+"px"; }
+			if(i<121) { block_object.style.top = 250+"px"; }
+			if(i<141) { block_object.style.top = 300+"px"; }
+			if(i<161) { block_object.style.top = 350+"px"; }
+			if(i<181) { block_object.style.top = 400+"px"; }
+			if(i<201) { block_object.style.top = 450+"px"; }
+			if(i<221) { block_object.style.top = 500+"px"; }
+			if(i<241) { block_object.style.top = 550+"px"; }
+			if(i<261) { block_object.style.top = 600+"px"; }
+			if(i<281) { block_object.style.top = 650+"px"; }
+			if(i<301) { block_object.style.top = 700+"px"; }
+			if(i<321) { block_object.style.top = 750+"px"; }
+			if(i<341) { block_object.style.top = 800+"px"; }
+			if(i<361) { block_object.style.top = 850+"px"; }
+			if(i<381) { block_object.style.top = 900+"px"; }
+			if(i<401) { block_object.style.top = 950+"px"; }
+		}
+	}
+}
